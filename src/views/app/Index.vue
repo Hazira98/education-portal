7f10c7f3f7bd2883ea34b8d0dec606ac106070f9
Hazira98 <64133752+Hazira98@users.noreply.github.com>
2020-06-12 17:04:03 +0800

    Create Index.vue

---
+++ src/views/app/Index.vue
@@ -0,0 +1,21 @@
+<template>
+    <a-layout>
+        <dashboard-core-drawer/>
+        <dashboard-core-view/>
+    </a-layout>
+</template>
+
+<script>
+    import DashboardCoreDrawer from "./component/core/Drawer";
+    // import View from "./component/core/View";
+    import DashboardCoreView from "./component/core/View";
+
+    export default {
+        name: "AppIndex",
+        components: {DashboardCoreView, DashboardCoreDrawer}
+    }
+</script>
+
+<style scoped>
+
+</style>
