b12a1a06250e298fd3533ae3037076f34ff8296a
Hazira98 <64133752+Hazira98@users.noreply.github.com>
2020-06-12 16:46:13 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,10 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import Antd from 'ant-design-vue'
+import store from './store'
+import 'ant-design-vue/dist/antd.css';
+import ReadMore from 'vue-read-more';
+
 
 Vue.config.productionTip = false
+Vue.use(ReadMore);
+Vue.use(Antd);
+
 
 new Vue({
   router,
-  render: h => h(App)
+  store,
+  render: h => h(App),
+  components: { App }
 }).$mount('#app')
+
