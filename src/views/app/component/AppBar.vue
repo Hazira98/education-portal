f9127c0d67cb45a52540499b290a212670548ad3
Hazira98 <64133752+Hazira98@users.noreply.github.com>
2020-06-13 08:32:23 +0800

    Update AppBar.vue

--- src/views/app/component/AppBar.vue
+++ src/views/app/component/AppBar.vue
@@ -1,5 +1,12 @@
 <template>
-    <div>Header</div>
+   <a-layout-header :style="{ background: '#A9F37F', padding: 0 }">
+        <a-row type="flex" justify="start">
+            <a-col :span="5" :offset="1" class=logo><p>University ABC</p></a-col>
+            <a-col :xs="{span:4 , offset:8}" :sm="{span:4 , offset:10}" :md="{span:3 , offset:12}" :lg="{span:2 , offset:13}" :xl="{span:2 , offset:14}"><a-button type="link" href=" ">About Us</a-button></a-col>
+            <a-col :xs="2" :sm="2" :md="2" :lg="2" :xl="2" > <a-button type="link" href="/hospital">Login</a-button></a-col>
+            
+        </a-row>
+    </a-layout-header>
 </template>
 
 <script>
@@ -9,5 +16,13 @@
 </script>
 
 <style scoped>
-
+.ant-btn-link
+{
+    color: white;
+}
+.logo 
+{
+    color: white;
+    font-size: 25px;
+}
 </style>
