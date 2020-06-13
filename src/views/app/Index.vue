ccdb5cd1d9a46855da4deaf1e758b4c08072ce4c
Hazira98 <64133752+Hazira98@users.noreply.github.com>
2020-06-13 08:32:21 +0800

    Update Index.vue

--- src/views/app/Index.vue
+++ src/views/app/Index.vue
@@ -1,18 +1,18 @@
 <template>
     <a-layout>
-        <dashboard-core-drawer/>
+ 
         <dashboard-core-view/>
     </a-layout>
 </template>
 
 <script>
-    import DashboardCoreDrawer from "./component/core/Drawer";
+
     // import View from "./component/core/View";
-    import DashboardCoreView from "./component/core/View";
+    import DashboardCoreView from "./component/View";
 
     export default {
         name: "AppIndex",
-        components: {DashboardCoreView, DashboardCoreDrawer}
+        components: {DashboardCoreView}
     }
 </script>
 
